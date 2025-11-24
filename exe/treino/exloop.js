376c000c73998e39a2b5a312d48dad38eee08068
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-24 18:46:03 -0300

    exloop

---
+++ exe/treino/exloop.js
@@ -0,0 +1,11 @@
+let contagem = 1;
+let frutas = ["Banana", "morango", "Abacaxi", "kiwi"];
+
+for (let x in frutas) {
+  console.log(frutas[x]);
+}
+
+while (contagem <= 100) {
+  console.log(`Número ${contagem}`);
+  contagem++;
+}
