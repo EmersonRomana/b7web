026f17a21dd932d41e2b71fe16590fa780216deb
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-25 12:19:01 -0300

    acrescetando o .sort()

--- exe/treino/treino-array01.js
+++ exe/treino/treino-array01.js
@@ -1,2 +1,6 @@
 let frutas = ["Banana", "uva", "Abacaxi", "Morango", "kiwi"];
-console.log(frutas.join(" - "));
+console.log(frutas.join(" - ")); // .join() atribui um separador nos elementos dentro do array
+
+const lista = ["leite", "açucar", "feijão", "tempero"];
+lista.sort(); // .sort() Troca a ordem do array
+console.log(lista);
