de8348f1b7fbc545492c1f561fef07b3c6b87db2
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-29 20:23:24 -0300

    comentários

---
+++ exe/treino/poo.js
@@ -0,0 +1,8 @@
+// 1. A CLASSE (O Molde)
+class Pessoa {
+  // 2. O CONSTRUCTOR (A Preparação)
+  constructor(nome, idade) {
+    this.name = nome; // Define a pessoa
+    this.idade = idade;
+  }
+}
