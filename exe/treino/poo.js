0ad96ca36d00d2dac5d9a374cfc9508123cc8146
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-29 21:37:00 -0300

    colocando anotações e mudando a estrutura

--- exe/treino/poo.js
+++ exe/treino/poo.js
@@ -1,8 +1,14 @@
 // 1. A CLASSE (O Molde)
 class Pessoa {
+  idade = 0;
+
   // 2. O CONSTRUCTOR (A Preparação)
-  constructor(nome, idade) {
-    this.name = nome; // Define a pessoa
-    this.idade = idade;
+  constructor(nome) {
+    this.nome = nome; // Define a pessoa
   }
 }
+
+const p1 = new Pessoa("Emerson");
+p1.idade = 33;
+
+console.log(`${p1.nome} tem ${p1.idade} anos`);
