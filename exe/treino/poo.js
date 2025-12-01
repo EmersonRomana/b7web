be0101f42fced99cc4488db486676dec4eed9804
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-30 22:15:11 -0300

    pessoas

--- exe/treino/poo.js
+++ exe/treino/poo.js
@@ -1,7 +1,7 @@
 // 1. A CLASSE (O Molde)
 class Pessoa {
   idade = 0;
-  pasoos = 0;
+  passos = 0;
 
   // 2. O CONSTRUCTOR  (informções que o objeto tem)
   constructor(nome) {
@@ -18,4 +18,5 @@ const p1 = new Pessoa("Emerson");
 const p2 = new Pessoa("Thay");
 const p3 = new Pessoa("Angelo");
 
-console.log(`${p1.nome} tem ${p1.idade} anos`);
+p1.contadorPassos();
+console.log(`Passos de ${p1.nome}: Foram ${p1.passos}`);
