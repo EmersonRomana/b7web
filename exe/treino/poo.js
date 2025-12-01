0fd18ba882ef51184192c02fd441663969511aee
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-11-30 22:02:51 -0300

    atualizando

--- exe/treino/poo.js
+++ exe/treino/poo.js
@@ -1,14 +1,21 @@
 // 1. A CLASSE (O Molde)
 class Pessoa {
   idade = 0;
+  pasoos = 0;
 
   // 2. O CONSTRUCTOR  (informções que o objeto tem)
   constructor(nome) {
     this.nome = nome; // Define a pessoa
   }
+
+  contadorPassos() {
+    this.passos++;
+  }
 }
 
-const p1 = new Pessoa("Emerson"); // Colocndo new e usando o nome da classe nos atribuimos Emerson como se fosse um objeto dando o nome e logo enseguida a idade também.
-p1.idade = 33;
+// Colocndo new e usando o nome da classe nos atribuimos Emerson como se fosse um objeto dando o nome e logo enseguida a idade também.
+const p1 = new Pessoa("Emerson");
+const p2 = new Pessoa("Thay");
+const p3 = new Pessoa("Angelo");
 
 console.log(`${p1.nome} tem ${p1.idade} anos`);
