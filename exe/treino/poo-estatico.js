beb2dbabc074eb32521abf2ef35765a4b9f83939
Emerson Romana Senna <133699469+EmersonRomana@users.noreply.github.com>
2025-12-05 19:01:14 -0300

    aula comentada

--- exe/treino/poo-estatico.js
+++ exe/treino/poo-estatico.js
@@ -1,7 +1,15 @@
 class Pessoa {
+  static maos = 2;
   idade = 0;
 
   constructor(nome) {
     this.nome = nome;
   }
+
+  digaOi() {
+    console.log(`Oi, eu sou o ${this.nome} e tenho ${Pessoa.maos} mãos`);
+  }
 }
+
+let p1 = new Pessoa("Emerson");
+p1.digaOi();
